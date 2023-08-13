17b4d0039bda077882867f705ee986a21fc671ed
Bᴀᴅsʜᴀ Sᴛᴜᴅɪᴏs <141768309+AkdBadsha@users.noreply.github.com>
2023-08-13 16:47:42 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -5,7 +5,7 @@ class Config:
     API_HASH = getenv("API_HASH", "b50fc1417139325122a146ef588ec212")
     BOT_TOKEN = getenv("BOT_TOKEN", "6342724900:AAE40893igCQNJ6fHsvLHnI8dQF3GBVdvW0")
     FSUB = getenv("FSUB", "")
-    CHID = int(getenv("CHID", ""))
+    CHID = int(getenv("CHID", "-100"))
     SUDO = list(map(int, getenv("SUDO").split()))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://autochannelfilter:autochannelfilter@cluster0.mcpsuva.mongodb.net/?retryWrites=true&w=majority")
     
